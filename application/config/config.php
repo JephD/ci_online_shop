28a70f2029117758a6915ff719cf31edea7a8e11
JephD <jephdavid@gmail.com>
2015-09-14 12:13:48 +0300

    updated the htacces and config files

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Mine/online_shop/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
